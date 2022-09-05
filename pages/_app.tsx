ac2813850ad68c57e7fd1c5f1c61152b1fe8d87b
Andreaz205 <andrei-bazunov@mail.ru>
2022-09-05 21:29:07 +0500

    first commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,27 @@
-import '../styles/globals.css'
+import 'antd/dist/antd.css'
 import type { AppProps } from 'next/app'
+import { QueryClient, QueryClientProvider } from 'react-query'
+
+import AuthProvider from '@/providers/AuthProvider'
+
+import '@/assets/styles/globals.scss'
+
+const queryClient = new QueryClient({
+	defaultOptions: {
+		queries: {
+			refetchOnWindowFocus: false
+		}
+	}
+})
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+	return (
+		<QueryClientProvider client={queryClient}>
+			<AuthProvider>
+				<Component {...pageProps} />
+			</AuthProvider>
+		</QueryClientProvider>
+	)
 }
 
 export default MyApp
